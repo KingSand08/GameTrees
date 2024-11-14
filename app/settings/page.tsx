ed9ac6254205124be07d6a6fb42b241ff0ae406f
Bellowsmay <Alimalex1525@gmail.com>
2024-11-13 20:26:04 -0800

    Minor changes to my settings page to match Steven's nav bar

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,48 +1,10 @@
 import Image from "next/image";
-
+import React, { useState } from 'react';
 // import { fetchDataFromDB } from "../lib/simpleDbFetch"
 // fetchDataFromDB();
 
 export default function SettingsPage() {
-  return (
-    <html>
-        <head>
-            <title>Account Settings</title>
-        </head>
-        <body> 
-        <section>
-            <div className="navbar bg-base-100">
-                <div className="flex-1">
-                    <a className="btn btn-ghost text-xl">GameManager+</a>
-                </div>
-                <div className="flex-none gap-2">
-                    <div className="form-control">
-                        <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-                    </div>
-                    <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img
-                                alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                            </div>
-                        </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            <li>
-                                <a className="justify-between">
-                                    Profile
-                                    <span className="badge">New</span>
-                                </a>
-                            </li>
-                            <li><a>Settings</a></li>
-                            <li><a>Logout</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </section>
+  return (    
         <section className="flex">
             <ul className="menu menu-md bg-base-200 w-56 rounded-box">
                 <li><a className="active">Account Settings</a></li>
@@ -53,20 +15,21 @@ export default function SettingsPage() {
             </ul>
             <section className="setting-content lg">
                 <h1 className="text-4xl">Account Settings</h1>
-                <div className="label-and-text-field">
+                <div className="avatar btn btn-lg btn-circle settings-space lg-pfp">
+                    <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
+                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" className=" hover:opacity-75"/>
+                    </div>
+                </div>
+                <div className="label-and-text-field settings-space">
                     <label>Username</label>
                     <input type="text" placeholder="Username" className="input input-bordered w-full max-w-xs" />
                 </div>
-                <div className="label-and-text-field">
+                <div className="label-and-text-field settings-space">
                     <label>Email</label>
                     <input type="text" placeholder="Email" className="input input-bordered w-full max-w-xs" />
                 </div>
                 <button className="btn btn-primary">Save</button>
             </section>
         </section>
-        
-        </body>
-        
-    </html>
 );
 }
