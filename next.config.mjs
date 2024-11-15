2a89972bf21293a53246e81e96645f5023f09cdc
Bellowsmay <Alimalex1525@gmail.com>
2024-11-14 18:50:30 -0800

    account settings page branch

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<<<< Temporary merge branch 1
   images: {
     domains: ["avatars.githubusercontent.com"], // Allow this domain for images
     domains: ["lh3.googleusercontent.com"], // Allow this domain for images
   },
-=========
     images: {
         domains: ["img.daisyui.com"],
     },
-
->>>>>>>>> Temporary merge branch 2
 };
 
 export default nextConfig;
