9083d37a351097366e0e613abb7d9588305c19ce
kingsand <clinvil2@gmail.com>
2024-12-05 13:27:37 -0800

    Theme fixes to sign in and out to fit new theme swtiching. And added background template now for sign in/sign up.

--- app/(site)/login/page.tsx
+++ app/(site)/login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from 'react'
 import Signin from '@/app/ui/components/auth/SigninPage'
+import styles from "@/app/ui/styles/Animations/AnimatedBackground.module.css";
 
 type Props = {
     searchParams?: Record<"callbackUrl" | "error", string>;
@@ -10,12 +11,22 @@ type Props = {
 const Page = (props: Props) => {
 
     return (
-        <div className='flex justify-center m-0 md:m-[2em] min-h-screen'>
-            <div className='w-screen md:w-3/4 lg:w-1/2 p-6 bg-gray-800 rounded-lg shadow-lg h-fit'>
-                <Signin
-                    error={props.searchParams?.error}
-                    callbackUrl={props.searchParams?.callbackUrl}
-                />
+        <div className="relative min-h-screen inset-0">
+            {/* Background Design */}
+            <div
+                className={`h-screen w-screen absolute inset-0 bg-gradient-to-br from-purple-500 via-indigo-700 to-blue-500 -m-8
+                            ${styles["animate-gradient"]} bg-[length:400%_400%]`
+                }
+            ></div>
+            <div className="absolute inset-0 mix-blend-overlay bg-[url('/animations/patterns/dots-wave-animated.svg')] opacity-15 -m-8"></div>
+
+            <div className='relative z-10 flex justify-center m-0 md:m-[2em] min-h-screen'>
+                <div className='w-screen md:w-3/4 lg:w-1/2 p-6 dark:bg-gradient-to-br dark:from-slate-800 dark:to-slate-900 bg-slate-800 rounded-lg shadow-lg h-fit'>
+                    <Signin
+                        error={props.searchParams?.error}
+                        callbackUrl={props.searchParams?.callbackUrl}
+                    />
+                </div>
             </div>
         </div>
     )
--- app/ui/components/auth/SigninPage.tsx
+++ app/ui/components/auth/SigninPage.tsx
@@ -58,13 +58,13 @@ const Signin = (props: Props) => {
 
     return (
         <div className="max-w-screen-sm mx-auto py-6">
-            <h1 className="text-center text-2xl font-semibold bg-gradient-to-b rounded-md">
+            <h1 className="text-white text-center text-2xl font-semibold rounded-md">
                 Sign In
             </h1>
 
             {/* Success Message */}
             {successMsg ? (
-                <div className="text-white py-2 mt-4">
+                <div className="text-base-content py-2 mt-4">
                     <div className="opacity-75 flex justify-center text-center bg-green-600 rounded-lg w-full py-2 px-4 space-x-4">
                         <p className="text-white">{successMsg}</p>
                         <span className="loading loading-spinner loading-md"></span>
@@ -76,15 +76,14 @@ const Signin = (props: Props) => {
 
                 {/* Email Input */}
                 <div className="flex flex-col space-y-2">
-                    <label htmlFor="email" className="text-white text-md font-medium">
+                    <label htmlFor="email" className="text-base-content text-md font-medium">
                         Email:
                     </label>
-                    <div className="input input-bordered flex items-center gap-2 w-full bg-slate-700 rounded-lg">
+                    <div className="input input-bordered flex items-center gap-2 w-full bg-slate-300 dark:bg-slate-700 rounded-lg">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
+                            className="h-4 w-4 opacity-70 fill-black dark:fill-current"
                             viewBox="0 0 16 16"
-                            fill="currentColor"
-                            className="h-4 w-4 opacity-70"
                         >
                             <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
                             <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
@@ -95,46 +94,47 @@ const Signin = (props: Props) => {
                             name="email"
                             placeholder="Enter email"
                             onChange={(e) => (email.current = e.target.value)}
-                            className="bg-transparent placeholder-gray-400 p-2 w-full focus:outline-none"
+                            className="bg-transparent text-black dark:text-white placeholder-gray-700 dark:placeholder-gray-400 p-2 w-full focus:outline-none"
                         />
                     </div>
                 </div>
 
                 {/* Password Input */}
                 <div className="flex flex-col space-y-2">
-                    <label htmlFor="password" className="text-white text-md font-medium">
+                    <label htmlFor="password" className="text-base-content text-md font-medium">
                         Password:
                     </label>
-                    <div className="input input-bordered flex items-center gap-2 w-full bg-slate-700 rounded-lg">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 16 16"
-                            fill="currentColor"
-                            className="h-4 w-4 opacity-70"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                                clipRule="evenodd"
+                    <div className="flex flex-col space-y-4">
+                        <div className="input input-bordered flex items-center gap-2 w-full bg-slate-300 dark:bg-slate-700 rounded-lg">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 16 16"
+                                className="h-4 w-4 opacity-70 fill-black dark:fill-current"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                id="password"
+                                name="password"
+                                placeholder="Enter password"
+                                className="bg-transparent text-black dark:text-white placeholder-gray-700 dark:placeholder-gray-400 p-2 w-full focus:outline-none"
+                                autoComplete="off"
+                                onChange={(e) => (password.current = e.target.value)}
                             />
-                        </svg>
-                        <input
-                            type={showPassword ? "text" : "password"}
-                            id="password"
-                            name="password"
-                            placeholder="Enter password"
-                            className="bg-transparent placeholder-gray-400 p-2 w-full focus:outline-none"
-                            autoComplete="off"
-                            onChange={(e) => (password.current = e.target.value)}
-                        />
+                        </div>
+                        <button
+                            type="button"
+                            className="w-full dark:text-slate-200 text-md mx-auto px-7 py-3 rounded-xl bg-black bg-opacity-35 hover:bg-opacity-45 dark:bg-opacity-55 dark:hover:bg-opacity-65"
+                            onClick={() => setShowPassword((prev) => !prev)}
+                        >
+                            {showPassword ? "Hide Password" : "Show Password"}
+                        </button>
                     </div>
-                    <button
-                        type="button"
-                        className="w-full text-slate-200 text-md bg-black bg-opacity-45 px-4 py-2 rounded-lg hover:bg-opacity-55"
-                        onClick={() => setShowPassword((prev) => !prev)}
-                    >
-                        {showPassword ? "Hide Password" : "Show Password"}
-                    </button>
                 </div>
 
                 {/* Buttons */}
